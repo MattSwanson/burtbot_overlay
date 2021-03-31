491ee472735ba400d5022d0ceb958327d474e1f4
Matt Swanson <swanny11@gmail.com>
2021-03-30 19:59:01 -0500

    start emotes

--- marquee.go
+++ marquee.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
 	"image"
 	"image/color"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/MattSwanson/ebiten/v2"
 	"github.com/MattSwanson/ebiten/v2/text"
@@ -22,6 +27,20 @@ const (
 	regYOffset = -5
 )
 
+type marqueeMsg struct {
+	RawMessage string `json:"rawMessage"`
+	Emotes     string `json:"emotes"`
+}
+
+type emoteInfo struct {
+	indices []emoteIndex
+	img     *ebiten.Image
+}
+type emoteIndex struct {
+	start int
+	end   int
+}
+
 type Marquee struct {
 	on          bool
 	speed       float64
@@ -33,6 +52,7 @@ type Marquee struct {
 	color       color.RGBA
 	currentFont *font.Face
 	oneShot     bool
+	emotes      map[string]emoteInfo
 }
 
 func init() {
@@ -80,9 +100,49 @@ func (m *Marquee) enable(b bool) {
 	m.on = b
 }
 
-func (m *Marquee) setText(s string) {
-	m.text = s
-	m.textBounds = text.BoundString(*m.currentFont, s)
+// setText takes a json string that decodes to a marqueeMsg struct
+func (m *Marquee) setText(j string) {
+	msg := marqueeMsg{}
+	err := json.Unmarshal([]byte(j), &msg)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	msgEmotes := map[string]emoteInfo{}
+	if msg.Emotes != "" {
+		//	2313213:14-30,42-58/
+		const prefixLen = 13
+		emoteData := strings.Split(msg.Emotes, "/")
+		//emotes := map[string][]emoteIndex{}
+		for _, e := range emoteData {
+			split := strings.Split(e, ":")
+			indices := strings.Split(split[1], ",")
+			eIndices := make([]emoteIndex, len(indices))
+			for k, i := range indices {
+				nums := strings.Split(i, "-")
+				start, err := strconv.Atoi(nums[0])
+				if err != nil {
+					log.Println(err.Error())
+				}
+				end, err := strconv.Atoi(nums[1])
+				if err != nil {
+					log.Println(err.Error())
+				}
+				eIndices[k] = emoteIndex{start, end}
+			}
+			img, err := getImageFromCDN(split[0])
+			if err != nil {
+				log.Fatal(err)
+			}
+			msgEmotes[split[0]] = emoteInfo{eIndices, img}
+			fmt.Println(msgEmotes)
+		}
+		m.text = msg.RawMessage
+	} else {
+		m.text = msg.RawMessage
+	}
+	m.emotes = msgEmotes
+	m.textBounds = text.BoundString(*m.currentFont, m.text)
 	// 0 to screenHeight - m.textBounds.Dy() + m.yOffset
 	m.y = rand.Intn(screenHeight-m.textBounds.Dy()) + m.textBounds.Dy()
 	m.color = color.RGBA{uint8(rand.Intn(255)), uint8(rand.Intn(255)), uint8(rand.Intn(255)), 0xff}
@@ -107,6 +167,12 @@ func (m *Marquee) Draw(screen *ebiten.Image) {
 		//text.Draw(screen, m.text, *m.currentFont, m.x+1, m.y+1, m.color)
 		text.Draw(screen, m.text, *m.currentFont, int(m.x), m.y, m.color)
 	}
+	if len(m.emotes) > 0 {
+		for _, em := range m.emotes {
+			//em.img.DrawImage(screen, nil)
+			screen.DrawImage(em.img, nil)
+		}
+	}
 }
 
 func (m *Marquee) Embiggen() {
@@ -125,4 +191,20 @@ func (m *Marquee) Smol() {
 
 func (m *Marquee) SetSpeed(speed float64) {
 	m.speed = speed
+}
+
+func getImageFromCDN(id string) (*ebiten.Image, error) {
+	url := fmt.Sprintf("http://static-cdn.jtvnw.net/emoticons/v1/%s/3.0", id)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	img, _, err := image.Decode(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return ebiten.NewImageFromImage(img), nil
 }
